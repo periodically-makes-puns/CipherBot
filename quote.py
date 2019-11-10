48774cb5c295928fe5b0dfcf24093fa1ac1f1a07
Sundhararajan Vinodh Sangeetha <sundharvs@gmail.com>
2019-11-10 15:30:59 -0500

    Initial Discord Cogs

--- quote.py
+++ quote.py
@@ -1,6 +1,6 @@
 import random
 
 def getQuote():
-    file = "quotes.txt"
+    file = "data/quotes.txt"
     quotes = open(file, "r", encoding="utf-8").read().split("\n")
     return random.choice(quotes)
