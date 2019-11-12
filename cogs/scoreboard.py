31035ba5fc989ab457b313e18ddae373e25b50e9
Sundhararajan Vinodh Sangeetha <sundharvs@gmail.com>
2019-11-11 21:57:19 -0500

    bug fixes

--- cogs/scoreboard.py
+++ cogs/scoreboard.py
@@ -15,7 +15,7 @@ class Scoreboard(commands.Cog):
             await ctx.send("Not enough people have played yet for a leaderboard to form")
         else:
             scores = scores[:5]
-            await ctx.send(f'```1. {ctx.bot.fetch_user(scores[0][0]).name}\n2. {ctx.bot.get_user(scores[1][0]).name}\n3. {ctx.bot.get_user(scores[2][0]).name}\n4. {ctx.bot.get_user(scores[3][0]).name}\n5. {ctx.bot.get_user(scores[4][0]).name}\n```')
+            await ctx.send(f'```1. {ctx.bot.get_user(scores[0][0]).name}\n2. {ctx.bot.get_user(scores[1][0]).name}\n3. {ctx.bot.get_user(scores[2][0]).name}\n4. {ctx.bot.get_user(scores[3][0]).name}\n5. {ctx.bot.get_user(scores[4][0]).name}\n```')
 
 def setup(bot):
     bot.add_cog(Scoreboard(bot))
