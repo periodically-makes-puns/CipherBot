8668c451b9216de157377a3961dbbaf8ef67ab6b
Sundhararajan Vinodh Sangeetha <sundharvs@users.noreply.github.com>
2019-11-12 15:08:51 -0500

    bug: scoreboard fix

---
+++ cogs/scoreboard.py
@@ -0,0 +1,23 @@
+import discord
+from discord.ext import commands
+from data import db
+from operator import itemgetter
+
+class Scoreboard(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+    
+    @commands.command()
+    async def scoreboard(self,ctx):
+        leaderboard = f'```'
+        scores = db.readallscores()
+        scores.sort(key = itemgetter(1), reverse = True)
+        scores = scores[:5]
+        for index, score in scores:
+            leaderboard += f'\n{index + 1}. {ctx.bot.get_user(scores[index][0]).name}'
+        
+        leaderboard += f'```'
+        await ctx.send(leaderboard)
+
+def setup(bot):
+    bot.add_cog(Scoreboard(bot))
