e88972d32d44f58085daf03377b5da390bb02b7c
Eric Yang <eyang10000@gmail.com>
2019-11-12 20:43:58 -0500

    Add semicolons to SQL statements, use if/else check instead of two fetchone

--- data/db.py
+++ data/db.py
@@ -2,36 +2,34 @@ import sqlite3
 
 def readplaintext(userid):
     conn = sqlite3.connect('data/users.db')
-    cursor = conn.execute("SELECT * FROM Users WHERE USERID = ?", (userid,))
+    cursor = conn.execute("SELECT * FROM Users WHERE USERID = ?;", (userid,))
     for row in cursor:
         return row[1]
 
 def writeplaintext(userid,plaintext):
     conn = sqlite3.connect('data/users.db')
-    cursor = conn.execute("SELECT * FROM Users WHERE USERID = ?", (userid,))
-    if(cursor.fetchone != None):
-        conn.execute("UPDATE Users SET PLAINTEXT = ? WHERE USERID = ?", (plaintext, userid))
-    if(cursor.fetchone() == None):
+    cursor = conn.execute("SELECT * FROM Users WHERE USERID = ?;", (userid,))
+    if cursor.fetchone() != None:
+        conn.execute("UPDATE Users SET PLAINTEXT = ? WHERE USERID = ?;", (plaintext, userid))
+    else:
         conn.execute("INSERT INTO Users (USERID,PLAINTEXT,SCORE) VALUES (?, ?, ?);", (userid, plaintext, 0))
-    
     conn.commit()
 
 def changescore(userid,increment):
     conn = sqlite3.connect('data/users.db')
 
-    cursor = conn.execute("SELECT * FROM Users WHERE USERID = ?", (userid,))
+    cursor = conn.execute("SELECT * FROM Users WHERE USERID = ?;", (userid,))
     for row in cursor:
         score = row[2]
     
     score += increment
-    conn.execute("UPDATE Users SET SCORE = ? WHERE USERID = ?", (score, userid))
+    conn.execute("UPDATE Users SET SCORE = ? WHERE USERID = ?;", (score, userid))
     conn.commit()
 
 def readallscores():
     scores = []
     conn = sqlite3.connect('data/users.db')
-    cursor = conn.execute("SELECT * FROM Users")
+    cursor = conn.execute("SELECT * FROM Users;")
     for row in cursor:
         scores.append([row[0],row[2]])
-    
     return scores
